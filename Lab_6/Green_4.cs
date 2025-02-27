2df4d6ee2a3e2b3a7cb785b318491391031078ce
magistr-lesa <kirillorinbaev@gmail.com>
2025-02-27 10:38:12 +0300

    Update Green_4.cs

--- Lab_6/Green_4.cs
+++ Lab_6/Green_4.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace Lab_6
 {
     public class Green_4
     {
@@ -16,15 +16,15 @@ namespace ConsoleApp2
             private double[] _jumps; // Прыжки.
             private double _bestJump; // Объявляем лучший прыжок.
             private int _index = 0;
-            public readonly string Name // Свойство имени.
+            public string Name // Свойство имени.
             {
-                get { return _name ?? ""; }
+                get { return _name; }
             }
-            public readonly string Surname // Свойство фамилии.
+            public string Surname // Свойство фамилии.
             {
-                get { return _surname ?? ""; }
+                get { return _surname; }
             }
-            public readonly double[] Jumps // Свойство прыжков.
+            public double[] Jumps // Свойство прыжков.
             {
                 get
                 {
@@ -40,14 +40,14 @@ namespace ConsoleApp2
             }
             public Participant(string name, string surname) // Публичный конструктор, принимающий имя и фамилию.
             {
-                _name = name ?? "";
-                _surname = surname ?? "";
+                _name = name;
+                _surname = surname;
                 _jumps = new double[3];
                 _bestJump = 0;
             }
             public void Jump(double result) // Прыыыыжок.
             {
-                if (_jumps.Length == 0 || _jumps == null) return;
+                if (_jumps == null || _jumps.Length == 0) return;
                 if (_index < 3 && result >= 0)
                 {
                     _jumps[_index] = result;
@@ -56,24 +56,24 @@ namespace ConsoleApp2
             }
             public static void Sort(Participant[] array) // Статический метод сортировки массива прыгунов по убыванию лучшего прыжка.
             {
-                if (array == null)
+                if (array == null || array.Length == 0)
                     return;
-                Array.Sort(array, (p1, p2) => p2.BestJump.CompareTo(p1.BestJump));
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[i].BestJump < array[j].BestJump || (array[i].BestJump == array[j].BestJump && i > j))
+                        {
+                            Participant temp = array[i];
+                            array[i] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
             }
             public void Print() // Публичный метод для вывода информации о необходимых полях структуры.
             {
                 Console.WriteLine("{0,-12} {1,-10} {2,-15:F2}", Name, Surname, BestJump);
-            }
-            public static void PrintAll(Participant[] participants) // Публично-статичный метод для полного вывода.
-            {
-                Console.WriteLine("{0,-12} {1,-10} {2,-15}", "Name", "Surname", "The best jump");
-                Console.WriteLine(new string('—', 60));
-
-                foreach (var p in participants)
-                {
-                    p.Print();
-                }
-                Console.WriteLine(new string('—', 60));
             }
         }
     }
